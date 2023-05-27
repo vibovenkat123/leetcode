00a54e41355b43a779023cd032db681a3aef80ad
Vaibhav Venkat <vibovenkat@gmail.com>
2023-05-26 20:41:13 -0500

    fix: remove bundled js

--- misc/len_last_word/main.js
+++
@@ -1,7 +0,0 @@
-function lengthOfLastWord(s) {
-    s = s.trim();
-    const words = s.split(" ");
-    return words[words.length - 1].length;
-}
-;
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
